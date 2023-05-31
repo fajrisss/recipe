2482ce9213044ed5fce3afd3cd2e6e7b88838071
Fajrisshoba <67568294+fajrisss@users.noreply.github.com>
2023-06-01 00:07:00 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/jsx-no-target-blank */
 import Filter from "./pages/filter/Filter"
-import Todo from "./pages/todo/Todo"
 
 
 function App() {
